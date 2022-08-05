c1ffc6317b1005c08dbe4a7988d66487b7cf42fb
Patrick Melo <patrickabimael@yahoo.com.br>
2022-08-05 11:40:09 -0300

    cria função de proibir tweets idênticos no mesmo dia

--- backend/api/controllers/posts.controller.js
+++ backend/api/controllers/posts.controller.js
@@ -1,6 +1,12 @@
 import {client} from './database.ts'
 import { stringForPost } from './settingQueries.js';
 
+const checkingEqualTweets = function (tweetToPost, tweetFound) {
+    // Os indexes 8 e 9 das strings de timestamp são os correspondentes ao dia
+    // Então se essa função retornar true, significa que os tweets foram postados no mesmo dia.
+    return tweetToPost[8] + tweetToPost[9] === tweetFound[8] + tweetToPost[9]
+}
+
 export const getTweets = async function (ctx) {
     try {
         const result = await client.queryObject('SELECT * FROM public.posts;')
@@ -29,10 +35,28 @@ export const getTweets = async function (ctx) {
 export const createPost = async function (ctx) {
     try {
         const tweet = await ctx.request.body().value
-        const [keys, values] = await stringForPost(tweet)
-        await client.queryObject(`INSERT INTO public.posts (${keys}) VALUES (${values});`)
-        ctx.response.status = 201
-        ctx.response.body = {message : "Tweetado!"}
+        const {post_owner_id, content} = tweet
+        const {rows} = await client.queryObject(`SELECT * FROM public.posts WHERE post_owner_id='${post_owner_id}' AND content='${content}';`)
+
+        if (rows.length === 0) {
+            const [keys, values] = await stringForPost(tweet)
+            await client.queryObject(`INSERT INTO public.posts (${keys}) VALUES (${values});`)
+            ctx.response.status = 201
+            ctx.response.body = {message : "Tweetado!"}
+        }
+        else {
+            rows.forEach(tweet => {
+                if (checkingEqualTweets(content, tweet.content)) {
+                    ctx.response.status = 200
+                    ctx.response.body = {message : "Você já twittou isso!"}
+                    return
+                }
+            })
+
+        }
+
+
+
     } catch (error) {
         ctx.response.status = 200
         ctx.response.body = {message : "Não foi possível twittar!"}
@@ -43,8 +67,8 @@ export const createPost = async function (ctx) {
 export const removePost = async function(ctx) {
     try {
         await client.queryObject(`DELETE FROM public.posts WHERE post_id='${ctx.params.post_id}'`)
-        ctx.response.body = {message: "Usuário exluído com sucesso!"}
-        ctx.response.status = 204
+        ctx.response.body = {message: "Tweet exluído com sucesso!"}
+        ctx.response.status = 200
     }
     catch (error) {
         ctx.response.body = {message: "Não foi possível deletar o tweet"}
