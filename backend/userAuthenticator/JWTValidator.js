631cad5163d226dcc152e39e3aad1b647cb58a59
Patrick Melo <patrickabimael@yahoo.com.br>
2022-08-16 15:05:50 -0300

    refatora middleware do authenticator

--- backend/userAuthenticator/JWTValidator.js
+++ backend/userAuthenticator/JWTValidator.js
@@ -3,7 +3,6 @@ import { decode } from "https://deno.land/x/djwt@v2.7/mod.ts";
 
 export const JWTValidator = async function (ctx, next) {
     const {headers} = ctx.request
-    console.log('oieee')
     try {
         const jwt = headers.get('Authorization').split(' ')[1]
 
