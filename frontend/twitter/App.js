5c80e17a8a617a794e7db4d36a4f0d285ae5fce9
Patrick Melo <patrickabimael@yahoo.com.br>
2022-08-16 15:32:24 -0300

    inicia projeto react-native

--- frontend/twitter/App.js
+++ frontend/twitter/App.js
@@ -1,35 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component, useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { api } from './src/services/api';
-// import { user } from './../../backend/api/models/user';
 
-export default class App extends Component {
-
-  constructor(props){
-    super(props)
-    this.state = {
-      users: []
-    }
-  }
-
-  async componentDidMount(){
-    const {data: users} = await api.get('users')
-    this.setState({
-      users: users[0]
-    })
-    console.log(this.state)
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>{String(this.state.users.full_name)}</Text>
-        <Text> abaixo </Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
--- frontend/twitter/src/services/api.js
+++
@@ -1,6 +0,0 @@
-import {create} from 'axios'
-// import {create} from 'apisauce'
-
-export const api = create({baseURL: 'http://localhost:8080/api/'})
-
-// export const api = create({baseURL: 'https://pokeapi.co/api/v2/pokemon/'})
