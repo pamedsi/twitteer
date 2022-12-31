f21d64a36a027f01f63b4fb127cb25382030a3a4
Patrick Melo <patrickabimael@yahoo.com.br>
2022-12-31 11:52:01 -0300

    usa classe invés de interface para tipar o usuário

--- backend/api/models/user.ts
+++ backend/api/models/user.ts
@@ -1,20 +1,25 @@
 export interface userProperty {
-    key: 'full_name'| 'birth_date'| 'city'| 'phone'| 'email'| 'username'| 'social_name'| 'bio'| 'url_on_bio'| 'profile_pic'| 'cover_pic'
+    key: 'user_id' | 'full_name'| 'birth_date'| 'city'| 'phone'| 'email'| 'username'| 'social_name'| 'created_at' |'bio'| 'url_on_bio'| 'profile_pic'| 'cover_pic'
 }
 
-export interface userModel {
-    user_id: string
-	full_name: string,
-    birth_date: string,
-    city?: string,
-    phone?: string,
-    email: string,
-    username: string,
-    password: string,
-    social_name?: string,
-    profile_creation_date?: Date,
-    bio?: string
-    url_on_bio?: string,
-    profile_pic?: string,
-    cover_pic?: string
+export class User {
+    user_id?: string;
+	full_name: string;
+    birth_date: string;
+    city?: string;
+    phone?: string;
+    email: string;
+    username: string;
+    password: string;
+    social_name?: string;
+    created_at?: string
+    bio?: string;
+    url_on_bio?: string;
+    profile_pic?: string;
+    cover_pic?: string;
+
+    constructor() {
+        if (!this.user_id) this.user_id = crypto.randomUUID()
+        if (!this.created_at) this.created_at = new Date().toISOString()
+    }
 }
