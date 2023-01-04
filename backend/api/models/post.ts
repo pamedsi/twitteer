e1976a11695cb3b719380dbee2337285dd6677d6
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-04 10:51:33 -0300

    cria classe de post

--- backend/api/models/post.ts
+++ backend/api/models/post.ts
@@ -2,5 +2,19 @@ export interface postModel {
     post_id: string,
     post_owner_id: string,
     content: string,
-    post_datetime: Date,
+    created_at: string,
+}
+
+export class Post implements postModel {
+    post_id: string;
+    post_owner_id: string;
+    content: string;
+    created_at: string;
+
+    constructor(user_id: string, content: string) {
+        this.post_id = crypto.randomUUID()
+        this.post_owner_id = user_id
+        this.content = content
+        this.created_at = new Date().toISOString()
+    }
 }
