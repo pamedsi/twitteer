91a854273fb97b78386172e4a53a343245fb5ee1
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-11 21:35:02 -0300

    põe .trim() na requisição de inserir comentário

--- backend/src/utils/forTweets.ts
+++ backend/src/utils/forTweets.ts
@@ -4,13 +4,12 @@ import { Comment, commentModel } from './../models/comment.ts';
 
 export const insertNewTweet = async function ({post_id, content, created_at, post_owner_id}: Post) {
     const query = `INSERT INTO public.posts (post_id, content, created_at, post_owner_id) VALUES ('${post_id}', '${content}', '${created_at}', '${post_owner_id}');`
-    // console.log(query)
     await client.queryObject(query)
     console.log(`\nInserção no banco de dados feita.\nQuery:\n${query}`)
 }
 
 export const insertNewComment  = async function({comment_id, comment_owner_id, commented_post_id, content, created_at}: Comment) {
-    const query = `INSERT INTO public.comments (comment_id, comment_owner_id, commented_post_id, content, created_at) VALUES ('${comment_id}', '${comment_owner_id}', '${commented_post_id}', '${content}', '${created_at}');`
+    const query = `INSERT INTO public.comments (comment_id, comment_owner_id, commented_post_id, content, created_at) VALUES ('${comment_id}', '${comment_owner_id}', '${commented_post_id}', '${content.trim()}', '${created_at}');`
     await client.queryObject(query)
     console.log(`\nInserção no banco de dados feita.\nQuery:\n${query}`)
 }
