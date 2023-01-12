ff6a39baa08876cdf1c293a847eb5ede46667e6f
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-11 21:42:48 -0300

    cria regex para validações

---
+++ backend/src/assets/alphanumericRegex.ts
@@ -0,0 +1,1 @@
+export const alphaumericRegex = /[A-Z]|[0-9]/gi
---
+++ backend/src/assets/validPassword.ts
@@ -0,0 +1,4 @@
+export const uppercase = /[A-Z]/g
+export const lowercase = /[a-z]/g
+export const numbers = /[0-9]/g
+export const symbols = /(?:(?![A-Z]|[0-9]).)/gi
