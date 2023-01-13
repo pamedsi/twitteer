2ca40afbb98931610a251f640078f891344ec143
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-13 09:38:48 -0300

    implementa os services de criar e excluir comentários no controlador

--- backend/src/controllers/comments.controller.ts
+++ backend/src/controllers/comments.controller.ts
@@ -1,8 +1,9 @@
 import {client} from '../database/database.ts'
 import {Context} from "https://deno.land/x/oak@v10.6.0/mod.ts";
-import {User} from '../models/user.ts'
 import { ctxModel } from './../models/context.ts';
-import { CreateCommentService, ICommentRequest } from '../services/createCommentService.ts';
+import { CreateCommentService, ICommentRequest } from '../services/comments/createCommentService.ts';
+import { DeleteCommentService } from '../services/comments/deleteCommentService.ts'
+import { decode, Payload } from "https://deno.land/x/djwt@v2.7/mod.ts";
 
 export const getComments = async function (ctx: Context) {
     try {
@@ -21,29 +22,37 @@ export const getComments = async function (ctx: Context) {
 export const createComment = async function (ctx: Context) {
     try {
         const comment: ICommentRequest = await ctx.request.body().value
-        const {user_id: comment_owner_id}: User = ctx.state.user
-        const newCommentService = new CreateCommentService(comment_owner_id)
-        await newCommentService.execute(comment)
+        // const {user_id: comment_owner_id}: User = ctx.state.user
+        const jwt = await ctx.cookies.get('jwt')
+        if (!jwt) throw new Error("JWT Inválido!");
+
+        const {user_id: comment_owner_id} = decode(jwt)[1] as Payload
+        const newCommentService = new CreateCommentService()
+        await newCommentService.execute(comment, String(comment_owner_id))
+        
         ctx.response.status = 201
-        ctx.response.body = {message : "Twittado!"}
+        ctx.response.body = {message : "Comentado!"}
 
     } catch (error) {
         ctx.response.status = 200
-        ctx.response.body = {message : `Não foi possível twittar! ${(String(error).split('\n')[0]).replace('Error: ', '')}`}
-        console.log("Não foi possível twittar.\n", error)
+        ctx.response.body = {message : "Não foi possível comentar!"}
+        console.log("Não foi possível comentar.\n", error)
+        // ctx.response.body = {message : `Não foi possível twittar! ${(String(error).split('\n')[0]).replace('Error: ', '')}`}        
     }
 }
 
 export const removeComment = async function(ctx: ctxModel) {
     try {
         const {comment_id} = ctx.params
-        await client.queryObject(`DELETE FROM public.comments WHERE comment_id='${comment_id}'`)
+        const deleteCommentService = new DeleteCommentService()
+        await deleteCommentService.execute(String(comment_id))
+        
         ctx.response.body = {message: "Comentário exluído com sucesso!"}
         ctx.response.status = 200
     }
     catch (error) {
         ctx.response.body = {message: "Não foi possível deletar o comentário"}
         ctx.response.status = 200
-        console.log(`Não foi possível deletar o tweet.\n`, error)
+        console.log(`Não foi possível deletar o comentário.\n`, error)
     }
 }
