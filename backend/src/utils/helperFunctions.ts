3347ba380e2cf26ff2ac2ca8237f21fe2897923b
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-13 09:24:57 -0300

    implementa busca de usuário por ID na função "userExists"

--- backend/src/utils/helperFunctions.ts
+++ backend/src/utils/helperFunctions.ts
@@ -29,7 +29,14 @@ export const isLogged = async function (ctx: ctxModel, next: any) {
 
 }
 
-export const userExists = async function (login: string)  {
+export const userExists = async function (login: string, user_id?: string)  {
+
+  if(user_id) {
+    const {rows: result} = (await client.queryObject<User>(`SELECT * FROM public.users WHERE user_id ='${user_id}' LIMIT 1;`))
+
+    if (result.length) return {dataFound: 'user_id', userFound: result[0]} as querySearch
+    return false
+  }
 
   let kindOfLogin: string
 
