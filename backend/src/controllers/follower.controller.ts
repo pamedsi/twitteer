1d1f934a3d9dfc44280c7a19567f6a01689fc974
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-13 09:37:13 -0300

    implementa o service de follow no controlador

--- backend/src/controllers/follower.controller.ts
+++ backend/src/controllers/follower.controller.ts
@@ -1,53 +1,9 @@
-import {client} from '../database/database.ts'
-import {Context} from "https://deno.land/x/oak@v10.6.0/mod.ts";
-import { followerModel } from './../models/follower.ts';
+import { decode, Payload } from "https://deno.land/x/djwt@v2.7/mod.ts";
+import { client } from "../database/database.ts";
+import { ctxModel } from '../models/context.ts'
+import { FollowService } from "../services/follow/follow.ts";
 
-export const follow = async function (ctx: Context) {
-    try {
-        const {user_id: following_id} = ctx.state.user
-        const {followed_id} = await ctx.request.body().value
-        const {rows} = await client.queryObject(`SELECT * FROM public.followers WHERE followed_id='${followed_id}' AND following_id='${following_id}' LIMIT 1;`)
-
-        if (rows.length === 0) {
-            await client.queryObject(`INSERT INTO public.followers (followed_id, following_id, created_at) VALUES ('${followed_id}'::uuid,'${following_id}'::uuid, '${new Date().toISOString()}');`)
-            ctx.response.status = 201
-            ctx.response.body = {message : "Operação de seguir realizada com sucesso!"}
-        }
-        else {
-            ctx.response.status = 200
-            ctx.response.body = {message : "Este usuário já segue ao que você tentou seguir."}
-        }
-    }
-
-    catch (error) {
-            console.log("Não foi possível seguir o usuário\n", error)
-            ctx.response.body = {message : "Não foi possível seguir o usuário"}
-    }
-}
-
-export const unfollow = async function (ctx: Context) {
-    try {
-    const {user_id: following_id} = ctx.state.user
-    const {followed_id} = await ctx.request.body().value
-    const rows = (await client.queryObject(`SELECT * FROM public.followers WHERE followed_id='${followed_id}' AND following_id='${following_id}' LIMIT 1;`)).rows as followerModel[]
-
-    // Se o usuário que for dar unfollow não seguir o que será "desseguido".
-    if (rows.length === 0) {
-        ctx.response.body = {message: `Não foi possível deixar de seguí-lo. Verifique se este usuário, realmente segue quem você acredita seguir.`}
-        ctx.response.status = 200
-    }
-    else {
-        await client.queryObject(`DELETE FROM public.followers WHERE follow_id='${rows[0].follow_id}';`)
-        ctx.response.body = {message: "Usuário deixou de seguir com sucesso!"}
-        ctx.response.status = 200
-    }
-    }
-    catch(error) {
-        console.log(`Não foi deixar de seguir.\n`, error)
-    }
-}
-
-export const seeFollowers = async function (ctx: Context) {
+export const seeFollowers = async function (ctx: ctxModel) {
     try {
         const result = await client.queryObject('SELECT * FROM public.followers;')
         ctx.response.body = result.rows
@@ -59,3 +15,47 @@ export const seeFollowers = async function (ctx: Context) {
 
     }
 }
+
+export const follow = async function (ctx: ctxModel) {
+    try {
+        const jwt = await ctx.cookies.get('jwt')
+        if (!jwt) throw new Error("JWT Inválido!")
+        const { user_id: follower_id } = decode(jwt)[1] as Payload
+        const { followed_id } = ctx.params
+        
+        const followService = new FollowService()
+        followService.execute(String(follower_id), String(followed_id))
+
+        ctx.response.status = 201
+        ctx.response.body = {message : "Operação de seguir realizada com sucesso!"}
+
+    }
+
+    catch (error) {
+            console.log("Não foi possível seguir o usuário\n", error)
+            ctx.response.body = {message : "Não foi possível seguir o usuário"}
+    }
+}
+
+export const unfollow = function (ctx: ctxModel) {
+    try {
+    // const {user_id: following_id} = ctx.state.user
+    // const {followed_id} = await ctx.request.body().value
+    // const rows = (await client.queryObject(`SELECT * FROM public.followers WHERE followed_id='${followed_id}' AND following_id='${following_id}' LIMIT 1;`)).rows as followerModel[]
+
+    // // Se o usuário que for dar unfollow não seguir o que será "desseguido".
+    // if (rows.length === 0) {
+    //     ctx.response.body = {message: `Não foi possível deixar de seguí-lo. Verifique se este usuário, realmente segue quem você acredita seguir.`}
+    //     ctx.response.status = 200
+    // }
+    // else {
+    //     await client.queryObject(`DELETE FROM public.followers WHERE follow_id='${rows[0].follow_id}';`)
+    //     ctx.response.body = {message: "Usuário deixou de seguir com sucesso!"}
+    //     ctx.response.status = 200
+    // }
+    }
+    catch(error) {
+        console.log(`Não foi deixar de seguir.\n`, error)
+    }
+}
+
