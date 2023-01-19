8186150f0fbfc187da2a73669406360f90f53be1
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-19 13:46:58 -0300

    fix: corrige queries sql de criação de tabelas

--- backend/src/database/sql/followers.sql
+++ backend/src/database/sql/followers.sql
@@ -1,7 +1,8 @@
 CREATE TABLE public.followers (
-	follow_id uuid not null default uuid_generate_v4() primary key,
+	-- follow_id uuid not null default uuid_generate_v4() primary key,
+	follow_id uuid not null primary key,
 	followed_id uuid NOT NULL,
-	following_id uuid NOT NULL,
+	follower_id uuid NOT NULL,
 	created_at timestamp,
 	CONSTRAINT followers_fk FOREIGN KEY (followed_id) REFERENCES public.users(user_id),
 	CONSTRAINT followers_fk_1 FOREIGN KEY (following_id) REFERENCES public.users(user_id)
--- backend/src/database/sql/tweets.sql
+++ backend/src/database/sql/tweets.sql
@@ -1,9 +1,10 @@
 CREATE TABLE public.tweets (
     -- tweet_id uuid not null default uuid_generate_v4() primary key,
-    tweet_id uuid not null primary key
-    tweet_owner_id uuid not null, --chave estrangeira
-    content varchar not null,
-    created_at timestamp not null,
-    deleted boolean not null default false
-    CONSTRAINT tweets_fk FOREIGN KEY (tweet_owner_id) REFERENCES public.users(user_id)
+	tweet_id uuid NOT NULL PRIMARY KEY,
+	tweet_owner_id uuid NOT NULL,
+	"location" varchar,
+	"content" varchar NOT NULL,
+	created_at timestamp NOT NULL,
+	deleted boolean NOT NULL DEFAULT false,
+	CONSTRAINT tweets_fk FOREIGN KEY (tweet_owner_id) REFERENCES public.users(user_id)
 );
