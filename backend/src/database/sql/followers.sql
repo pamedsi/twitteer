fbe168f30e36d5bde3b39d60215487033cc30a5b
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-20 11:07:27 -0300

    fix: corrige constraint de chave estrangeira

--- backend/src/database/sql/followers.sql
+++ backend/src/database/sql/followers.sql
@@ -5,5 +5,5 @@ CREATE TABLE public.followers (
 	follower_id uuid NOT NULL,
 	created_at timestamp,
 	CONSTRAINT followers_fk FOREIGN KEY (followed_id) REFERENCES public.users(user_id),
-	CONSTRAINT followers_fk_1 FOREIGN KEY (following_id) REFERENCES public.users(user_id)
+	CONSTRAINT followers_fk_1 FOREIGN KEY (follower_id) REFERENCES public.users(user_id)
 );
