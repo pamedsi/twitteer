daa69ac155808d36b7bda72c87b240a9226ab60c
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-21 16:59:52 -0300

    feat: cria tabela de likes sql

---
+++ backend/src/database/sql/likes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public.likes (
+	like_id uuid not null primary key,
+	tweet_liked_id uuid NOT NULL,
+	user_who_liked uuid NOT NULL,
+	created_at timestamp,
+	CONSTRAINT like_fk FOREIGN KEY (tweet_liked_id) REFERENCES public.tweets(tweet_id),
+	CONSTRAINT like_fk1 FOREIGN KEY (tweet_liked_id) REFERENCES public.comments(comment_id),
+	CONSTRAINT like_fk2 FOREIGN KEY (user_who_liked) REFERENCES public.users(user_id)
+);
