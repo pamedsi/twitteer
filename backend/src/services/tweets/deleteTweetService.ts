91c7ef39022d58749b2d7870bd7aa63b7c9ccf43
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-23 21:44:50 -0300

    fix: corrige bug de autorizar apagar tweet

--- backend/src/services/tweets/deleteTweetService.ts
+++ backend/src/services/tweets/deleteTweetService.ts
@@ -8,7 +8,7 @@ export class DeleteTweetService {
       if (!validate(tweet_id, 4)) throw new Error("ID de tweet inválido!")
 
       const tweetFound = await tweetExists(tweet_id)
-      if (tweetFound && tweetFound.tweet_owner_id === tweet_owner_id) throw new Error("client: Você não é o dono deste tweet, portanto, não pode apagá-lo.")
+      if (tweetFound && tweetFound.tweet_owner_id !== tweet_owner_id) throw new Error("client: Você não é o dono deste tweet, portanto, não pode apagá-lo.")
 
       await deleteTweet(tweet_id)
   }
