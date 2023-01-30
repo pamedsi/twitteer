70ecb02277bf5449e141283d65c88a0f47e2aa38
Patrick Melo <patrickabimael@yahoo.com.br>
2023-01-30 16:28:57 -0300

    feat: linka banco de dados com docker

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import { router } from './routes/router.ts';
 import { env } from './env.ts';
 
-const {HOST, PORT} = env
+const { HOST, PORT } = env
 const app = new Application()
 
 app.use(router.routes())
--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,6 +1,5 @@
 import { Client } from 'https://deno.land/x/postgres@v0.17.0/mod.ts'
 import { env } from '../env.ts';
 
-const { user, database, hostname, port, password } = env
-export const client = new Client({user,database,hostname,port,password})
+export const client = new Client(env)
 await client.connect()
